2b7a0f6e1a57671a6d8c9ccd1545a5d46465057f
Erdoo Kuhwa <64602124+erdookuhwa@users.noreply.github.com>
2021-05-10 10:21:17 -0500

    clean up, made input req'd

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -11,12 +11,8 @@ function SearchMovies() {
     const searching = async (e) => {
         e.preventDefault();
 
-        // const query = "Jurassic Park"
-        // url
         // https://api.themoviedb.org/3/movie/550?api_key
         // myKey = 54f3169cc3a4bddd7d02aa523eba94bc
-
-        // const url = ``
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=54f3169cc3a4bddd7d02aa523eba94bc&language=en-US&query=${query}&page=1&include_adult=false`;
 
@@ -24,17 +20,13 @@ function SearchMovies() {
             const res = await fetch(url);
 
             const data = await res.json();
-            console.log(data.results)
 
             setMovies(data.results)
 
         } catch (err) {
             if (query.length < 1) {
-                console.log('No movie name matches your search!', err)
+                alert('No movie name matches your search!', err)
             }
-            
-
-            // CONSIDER ALERTING ERROR INSTEAD OF CONSOLE so user can see what went wrong... ALSO, PREVENT ABILITY TO SEARCH IF NOTHING IS ENTERED SO AN ERROR PAGE OF CANNOT READ PPTY OF UNDEFINED DOESN'T POP up.
         }
 
     }
@@ -45,7 +37,7 @@ function SearchMovies() {
         <>
             <form className="form" onSubmit={searching}>
                 <label className="label" htmlFor="query"><h2>Movie Name</h2></label>
-                <input className="input" type="text" name="query" placeholder="e.g. Wonder Woman" 
+                <input className="input" type="text" name="query" placeholder="e.g. Wonder Woman" required
                 value={query} onChange={(e) => setQuery(e.target.value)}
                 />
                 <button className="button" type="submit">Search</button>
